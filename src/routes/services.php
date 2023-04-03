ccc3cf1fc881040d61e341b1291147db1ebe5c24
snakepatch53 <snakepatch53@gmail.com>
2023-04-02 19:35:09 -0500

    Changes to be committed: 	modified:   src/routes/services.php

--- src/routes/services.php
+++ src/routes/services.php
@@ -7,62 +7,69 @@ $radapter->getHTML('/services/configuration', 'configuration');
 
 // INFO
 $radapter->post('/services/info/select', fn (...$args) => InfoService::select(...$args));
-$radapter->post('/services/info/update', fn (...$args) => InfoService::update(...$args));
+$radapter->post('/services/info/update', fn () => middlewareSessionServicesLogin(), fn (...$args) => InfoService::update(...$args));
 
 // USER
 $radapter->post('/services/user/login', fn (...$args) => UserService::login(...$args));
 $radapter->post('/services/user/logout', fn () => UserService::logout());
-$radapter->post('/services/user/select', fn (...$args) => UserService::select(...$args));
-$radapter->post('/services/user/insert', fn (...$args) => UserService::insert(...$args));
-$radapter->post('/services/user/update', fn (...$args) => UserService::update(...$args));
-$radapter->post('/services/user/delete', fn (...$args) => UserService::delete(...$args));
+// need to be logged
+$radapter->post('/services/user/select', fn () => middlewareSessionServicesLogin(), fn (...$args) => UserService::select(...$args));
+$radapter->post('/services/user/insert', fn () => middlewareSessionServicesLogin(), fn (...$args) => UserService::insert(...$args));
+$radapter->post('/services/user/update', fn () => middlewareSessionServicesLogin(), fn (...$args) => UserService::update(...$args));
+$radapter->post('/services/user/delete', fn () => middlewareSessionServicesLogin(), fn (...$args) => UserService::delete(...$args));
 
 // LINKS
 $radapter->post('/services/link/select', fn (...$args) => LinkService::select(...$args));
-$radapter->post('/services/link/insert', fn (...$args) => LinkService::insert(...$args));
-$radapter->post('/services/link/update', fn (...$args) => LinkService::update(...$args));
-$radapter->post('/services/link/delete', fn (...$args) => LinkService::delete(...$args));
+// need to be logged
+$radapter->post('/services/link/insert', fn () => middlewareSessionServicesLogin(), fn (...$args) => LinkService::insert(...$args));
+$radapter->post('/services/link/update', fn () => middlewareSessionServicesLogin(), fn (...$args) => LinkService::update(...$args));
+$radapter->post('/services/link/delete', fn () => middlewareSessionServicesLogin(), fn (...$args) => LinkService::delete(...$args));
 
 // SLIDER
 $radapter->post('/services/slider/select', fn (...$args) => SliderService::select(...$args));
-$radapter->post('/services/slider/insert', fn (...$args) => SliderService::insert(...$args));
-$radapter->post('/services/slider/update', fn (...$args) => SliderService::update(...$args));
-$radapter->post('/services/slider/delete', fn (...$args) => SliderService::delete(...$args));
+// need to be logged
+$radapter->post('/services/slider/insert', fn () => middlewareSessionServicesLogin(), fn (...$args) => SliderService::insert(...$args));
+$radapter->post('/services/slider/update', fn () => middlewareSessionServicesLogin(), fn (...$args) => SliderService::update(...$args));
+$radapter->post('/services/slider/delete', fn () => middlewareSessionServicesLogin(), fn (...$args) => SliderService::delete(...$args));
 
 // SOCIAL
 $radapter->post('/services/social/select', fn (...$args) => SocialService::select(...$args));
 // need to be logged
-$radapter->post('/services/social/insert', fn (...$args) => SocialService::insert(...$args));
-$radapter->post('/services/social/update', fn (...$args) => SocialService::update(...$args));
-$radapter->post('/services/social/delete', fn (...$args) => SocialService::delete(...$args));
+$radapter->post('/services/social/insert', fn () => middlewareSessionServicesLogin(), fn (...$args) => SocialService::insert(...$args));
+$radapter->post('/services/social/update', fn () => middlewareSessionServicesLogin(), fn (...$args) => SocialService::update(...$args));
+$radapter->post('/services/social/delete', fn () => middlewareSessionServicesLogin(), fn (...$args) => SocialService::delete(...$args));
 
 // MENSAJES
-$radapter->post('/services/mensaje/select', fn (...$args) => MensajeService::select(...$args));
 $radapter->post('/services/mensaje/insert', fn (...$args) => MensajeService::insert(...$args));
 // need to be logged
-// $radapter->post('/services/mensaje/update', fn (...$args) => MensajeService::update(...$args));
-$radapter->post('/services/mensaje/delete', fn () => fn (...$args) => MensajeService::delete(...$args));
+$radapter->post('/services/mensaje/select', fn () => middlewareSessionServicesLogin(), fn (...$args) => MensajeService::select(...$args));
+// $radapter->post('/services/mensaje/update', fn () => middlewareSessionServicesLogin(), fn (...$args) => MensajeService::update(...$args));
+$radapter->post('/services/mensaje/delete', fn () => middlewareSessionServicesLogin(), fn () => fn (...$args) => MensajeService::delete(...$args));
 
 // PLANES
 $radapter->post('/services/plan/select', fn (...$args) => PlanService::select(...$args));
-$radapter->post('/services/plan/insert', fn (...$args) => PlanService::insert(...$args));
-$radapter->post('/services/plan/update', fn (...$args) => PlanService::update(...$args));
-$radapter->post('/services/plan/delete', fn (...$args) => PlanService::delete(...$args));
+// need to be logged
+$radapter->post('/services/plan/insert', fn () => middlewareSessionServicesLogin(), fn (...$args) => PlanService::insert(...$args));
+$radapter->post('/services/plan/update', fn () => middlewareSessionServicesLogin(), fn (...$args) => PlanService::update(...$args));
+$radapter->post('/services/plan/delete', fn () => middlewareSessionServicesLogin(), fn (...$args) => PlanService::delete(...$args));
 
 // PREGUNTAS
 $radapter->post('/services/pregunta/select', fn (...$args) => PreguntaService::select(...$args));
-$radapter->post('/services/pregunta/insert', fn (...$args) => PreguntaService::insert(...$args));
-$radapter->post('/services/pregunta/update', fn (...$args) => PreguntaService::update(...$args));
-$radapter->post('/services/pregunta/delete', fn (...$args) => PreguntaService::delete(...$args));
+// need to be logged
+$radapter->post('/services/pregunta/insert', fn () => middlewareSessionServicesLogin(), fn (...$args) => PreguntaService::insert(...$args));
+$radapter->post('/services/pregunta/update', fn () => middlewareSessionServicesLogin(), fn (...$args) => PreguntaService::update(...$args));
+$radapter->post('/services/pregunta/delete', fn () => middlewareSessionServicesLogin(), fn (...$args) => PreguntaService::delete(...$args));
 
 // RESTRICCIONES
 $radapter->post('/services/restriccion/select', fn (...$args) => RestriccionService::select(...$args));
-$radapter->post('/services/restriccion/insert', fn (...$args) => RestriccionService::insert(...$args));
-$radapter->post('/services/restriccion/update', fn (...$args) => RestriccionService::update(...$args));
-$radapter->post('/services/restriccion/delete', fn (...$args) => RestriccionService::delete(...$args));
+// need to be logged
+$radapter->post('/services/restriccion/insert', fn () => middlewareSessionServicesLogin(), fn (...$args) => RestriccionService::insert(...$args));
+$radapter->post('/services/restriccion/update', fn () => middlewareSessionServicesLogin(), fn (...$args) => RestriccionService::update(...$args));
+$radapter->post('/services/restriccion/delete', fn () => middlewareSessionServicesLogin(), fn (...$args) => RestriccionService::delete(...$args));
 
 // REDES SOCIALES
 $radapter->post('/services/social/select', fn (...$args) => SocialService::select(...$args));
-$radapter->post('/services/social/insert', fn (...$args) => SocialService::insert(...$args));
-$radapter->post('/services/social/update', fn (...$args) => SocialService::update(...$args));
-$radapter->post('/services/social/delete', fn (...$args) => SocialService::delete(...$args));
+// need to be logged
+$radapter->post('/services/social/insert', fn () => middlewareSessionServicesLogin(), fn (...$args) => SocialService::insert(...$args));
+$radapter->post('/services/social/update', fn () => middlewareSessionServicesLogin(), fn (...$args) => SocialService::update(...$args));
+$radapter->post('/services/social/delete', fn () => middlewareSessionServicesLogin(), fn (...$args) => SocialService::delete(...$args));
