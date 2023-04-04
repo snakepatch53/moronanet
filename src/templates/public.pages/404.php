fa76d967acc6b23e395c80fb407c4c5f48d85eda
snakepatch53 <snakepatch53@gmail.com>
2023-04-04 16:45:13 -0500

    Changes to be committed: 	modified:   public/css.public/404.css 	modified:   src/templates/public.pages/404.php

--- src/templates/public.pages/404.php
+++ src/templates/public.pages/404.php
@@ -14,14 +14,48 @@
 
     <main class="animate__animated animate__fadeIn">
         <div class="container">
-            <img src="<?= $DATA['http_domain'] ?>public/img/404.png" alt="Ilustracion 404">
-            <h2>Oops, la página que buscas no existe.</h2>
-            <p>Parece que has llegado a una página que no está disponible o se ha eliminado.</p>
+            <img src="<?= $DATA['http_domain'] ?>public/img/personaje2.png?last=<?= $DATA['info']['info_last'] ?>" alt="Imagen 2 del personaje">
+            <h2>404</h2>
+            <p>Estas orbitando por página desconocidas.</p>
             <br><br>
             <a href="<?= $DATA['http_domain'] ?>">
-                <i class="fas fa-arrow-left"></i>
-                <span>Volver al inicio</span>
+                <i class="fas fa-globe-americas"></i>
+                <span>Regresame a la tierra</span>
             </a>
+        </div>
+
+
+        <div class="stars">
+            <div class="star star-1"></div>
+            <div class="star star-2"></div>
+            <div class="star star-3"></div>
+            <div class="star star-4"></div>
+            <div class="star star-5"></div>
+            <div class="star star-6"></div>
+            <div class="star star-7"></div>
+            <div class="star star-8"></div>
+            <div class="star star-9"></div>
+            <div class="star star-10"></div>
+            <div class="star star-11"></div>
+            <div class="star star-12"></div>
+            <div class="star star-13"></div>
+            <div class="star star-14"></div>
+            <div class="star star-15"></div>
+            <div class="star star-16"></div>
+            <div class="star star-17"></div>
+            <div class="star star-18"></div>
+            <div class="star star-19"></div>
+            <div class="star star-20"></div>
+            <div class="star star-21"></div>
+            <div class="star star-22"></div>
+            <div class="star star-23"></div>
+            <div class="star star-24"></div>
+            <div class="star star-25"></div>
+            <div class="star star-26"></div>
+            <div class="star star-27"></div>
+            <div class="star star-28"></div>
+            <div class="star star-29"></div>
+            <div class="star star-30"></div>
         </div>
 
     </main>
